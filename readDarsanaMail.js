0c4aa81fe632f32adbef32f7adeebbf0d0321060
oonishin <oonishin@gmail.com>
2014-12-18 15:44:30 +0900

    Create readDarsanaMail.js

---
+++ readDarsanaMail.js
@@ -0,0 +1,32 @@
+function readDarsanaMail(){
+  var sheet = SpreadsheetApp.getActiveSheet();
+  var thds = GmailApp.search("label:darsana")
+  var row = 1;
+
+  sheet.getRange(row,1).setValue("Date");
+  sheet.getRange(row,2).setValue("Portal");
+  sheet.getRange(row,3).setValue("Attack AgentID");
+  row++;
+  
+  for(var n in thds){
+      var thd = thds[n];
+      var msgs = thd.getMessages();
+      for(m in msgs){
+           var msg = msgs[m];
+           var from = msg.getFrom();
+           var date = msg.getDate();
+           var Subject = msg.getSubject();
+           var Agent = msg.getSubject().replace("Ingress Damage Report: Entities attacked by ", "");
+           var body = msg.getBody();
+           var Portal_temp = body.match(/\"Portal \-.*/);
+        　　　 Portal_temp = Portal_temp.join();
+               Portal_temp = Portal_temp.replace("\"Portal \- ","");
+               Portal = Portal_temp.replace(/\" height=\"160\".*/,"");
+
+           sheet.getRange(row,1).setValue(date);
+           sheet.getRange(row,2).setValue(Portal);
+           sheet.getRange(row,3).setValue(Agent);
+        row++;
+      }
+  }
+}
